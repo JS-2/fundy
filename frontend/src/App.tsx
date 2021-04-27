0dcd87e3a0550b5860ca0a3b6e0a8111d89fc63d
Wonchan <chany219@naver.com>
2021-04-27 15:41:11 +0900

    :bulb [ADD] Main Page 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,21 @@
 import { Box, Container } from '@material-ui/core';
 import Router from './router';
 import Navbar from './components/Navbar';
+import Main from './components/Main';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+
+
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
       <Box minWidth={1080} display="flex" justifyContent="center">
         <Box width={1080}>
+          <BrowserRouter>
+          <Route path="/" component={Main} />
+
+          </BrowserRouter>
           <Router></Router>
         </Box>
       </Box>
