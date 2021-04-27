fc68caf9f62031b42415255a4444b1295b7ef599
김재성 <kimjea23@naver.com>
2021-04-27 14:18:49 +0900

    :hammer: [EDIT] Update login UI

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,25 +1,67 @@
-import { Box, Checkbox, Container, Grid, TextField } from '@material-ui/core';
+import {
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  FormControlLabel,
+  Grid,
+  TextField,
+} from '@material-ui/core';
 import React, { useState } from 'react';
+import './login.css';
+import { User, loginSubmit } from '../api/user';
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [user, setUser] = useState<User>(new User());
 
   return (
     <Box display="flex" justifyContent="center">
-      <Box mt={10} width={720}>
+      <Box mt={10} width={480}>
         <Grid container spacing={3}>
-          <Grid item className="logo" xs={12}>
+          <Grid item className="logo login_logo mb-5" xs={12}>
             fundy
           </Grid>
           <Grid item xs={12}>
-            <TextField fullWidth label="아이디(이메일)" variant="outlined" />
+            <TextField
+              fullWidth
+              value={user.email}
+              onChange={(e) => {
+                setUser({ ...user, email: e.target.value });
+              }}
+              label="아이디(이메일)"
+              variant="outlined"
+            />
           </Grid>
           <Grid item xs={12}>
-            <TextField fullWidth label="비밀번호" variant="outlined" />
+            <TextField
+              fullWidth
+              value={user.password}
+              onChange={(e) => {
+                setUser({ ...user, password: e.target.value });
+              }}
+              type="password"
+              label="비밀번호"
+              variant="outlined"
+            />
           </Grid>
           <Grid item xs={12}>
-            <Checkbox></Checkbox>
+            <FormControlLabel
+              control={<Checkbox></Checkbox>}
+              label="아이디 저장"
+            ></FormControlLabel>
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              variant="contained"
+              size="large"
+              fullWidth
+              onClick={() => {
+                loginSubmit(user);
+              }}
+              className="btn_main nbg_bold"
+            >
+              로그인
+            </Button>
           </Grid>
         </Grid>
       </Box>
