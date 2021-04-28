0b0aa9079c23b10e911aebec194ec44ecc9790ab
김재성 <kimjea23@naver.com>
2021-04-28 22:54:00 +0900

    :hammer: [EDIT] Update signup UI #S04P31A303-37

--- frontend/src/api/user.ts
+++ frontend/src/api/user.ts
@@ -1,7 +1,8 @@
 export class User {
     email: string;
     password: string;
-  
+    nickName?: string;
+
     constructor(email: string = '', password: string = '') {
       this.email = email;
       this.password = password;
@@ -10,4 +11,22 @@ export class User {
 
 export const loginSubmit = (user: User) => {
     console.log(user);
+}
+
+export const registSubmit = (user: User) => {
+    console.log(user);
+}
+
+export const validateId = (id: string): boolean => {
+  let regExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+  if (regExp.test(id)) return true;
+  else return false;
+}
+
+export const validatePassword = (password: string): boolean => {
+  return password.length >= 8 ? true : false;
+}
+
+export const validateNickName = (nickName: string): boolean => {
+  return nickName.length >= 2 && nickName.length <= 16 ? true : false;
 }
