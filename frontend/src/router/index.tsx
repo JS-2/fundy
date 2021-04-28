8ee8667ae9915e4d3b2ba00741d4fbff7b9f1ece
윤혜민 <h2m0@swu.ac.kr>
2021-04-28 18:00:57 +0900

    :bulb: [ADD] create fundingCreate pages #S04P31A303-186 #S04P31A303-187 #S04P31A303-188

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from '../pages/Login';
+import Create from '../pages/funding/Create';
 const index = () => {
   return (
     <Router>
       <Switch>
         <Route path="/login" component={Login} exact />
+        <Route path="/funding/create" component={Create} exact />
       </Switch>
     </Router>
   );
