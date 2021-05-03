f374fcb1679d2ddebc538179c08658413113cc1c
김재성 <kimjea23@naver.com>
2021-05-03 10:59:37 +0900

    :bulb: [EDIT] Update IdolDetail Components #S04P31A303-67

---
+++ frontend/src/pages/idol/IdolDetail.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import IdolInfo from '../../components/idol/idolDetail/IdolInfo';
+
+const IdolDetail = () => {
+  return (
+    <div>
+      <IdolInfo />
+    </div>
+  );
+};
+
+export default IdolDetail;
