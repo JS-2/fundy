5538823bf2e9c6104a58613c9df33bcf516a746f
이나영 <bokiri16@gmail.com>
2021-05-05 18:36:44 +0900

    💡 [ADD] create FundingComment api #S04P31A303-168

---
+++ backend/src/main/java/com/ilovefundy/dao/FundingCommentDao.java
@@ -0,0 +1,8 @@
+package com.ilovefundy.dao;
+
+import com.ilovefundy.dto.funding.FundingComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FundingCommentDao extends JpaRepository<FundingComment, Integer> {
+
+}
