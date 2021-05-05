dae572d776cada14b01fd322c678799c5999efe7
이나영 <bokiri16@gmail.com>
2021-05-05 17:06:22 +0900

    🔨 [EDIT] edit FundingNotice files #S04P31A303-165

--- backend/src/main/java/com/ilovefundy/controller/FundingNoticeController.java
+++ backend/src/main/java/com/ilovefundy/controller/FundingNoticeController.java
@@ -1,12 +1,31 @@
 package com.ilovefundy.controller;
 
+import com.ilovefundy.dto.funding.FundingNotice;
+import com.ilovefundy.service.FundingNoticeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin(origins = {"*"})
 @RequiredArgsConstructor
 @RestController
 public class FundingNoticeController {
+    private final FundingNoticeService fundingNoticeService;
 
+    //전체 펀딩 공지사항 리스트
+    @GetMapping("/fundings/{funding_id}/notice")
+    public ResponseEntity<Object> fundingNoticeList(@RequestParam(defaultValue = "1") int page, int per_page, @RequestParam(required = false) String keyword){
+        List<FundingNotice> fundingNoticeList = fundingNoticeService.getFundingNoticeList(page-1, per_page);
+        return new ResponseEntity<>(fundingNoticeList, HttpStatus.OK);
+    }
+
+    //펀딩 공지사항 상세보기
+    @GetMapping("/fundings/{funding_id}/notices/{funding_notice_id}")
+    public ResponseEntity<Object> fundingInfo(@PathVariable int funding_notice_id){
+        FundingNotice fundingNotice = fundingNoticeService.getFundingNotice(funding_notice_id);
+        return new ResponseEntity<>(fundingNotice, HttpStatus.OK);
+    }
 }
--- backend/src/main/java/com/ilovefundy/service/FundingNoticeService.java
+++ backend/src/main/java/com/ilovefundy/service/FundingNoticeService.java
@@ -1,13 +1,27 @@
 package com.ilovefundy.service;
 
 import com.ilovefundy.dao.FundingNoticeDao;
+import com.ilovefundy.dto.funding.FundingNotice;
+import com.ilovefundy.dto.funding.FundingProject;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @CrossOrigin(origins = {"*"})
 @RequiredArgsConstructor
 @RestController
 public class FundingNoticeService {
     private final FundingNoticeDao fundingNoticeDao;
+
+    public List<FundingNotice> getFundingNoticeList(int page, int per_page) {
+        Page<FundingNotice> pages = fundingNoticeDao.findAll(PageRequest.of(page, per_page));
+        System.out.println(pages.getContent());
+        return pages.getContent();
+    }
+
+    public FundingNotice getFundingNotice(int id) { return fundingNoticeDao.findByFundingNoticeId(id); }
 }
