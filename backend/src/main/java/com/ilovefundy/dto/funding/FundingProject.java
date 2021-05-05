351de0780c1ba3d536289825594ef62dd1286d0b
Yunsung <msmf3@naver.com>
2021-05-06 01:32:42 +0900

    💡 [ADD] Swagger in User API #S04P31A303-25

--- backend/src/main/java/com/ilovefundy/dto/funding/FundingProject.java
+++ backend/src/main/java/com/ilovefundy/dto/funding/FundingProject.java
@@ -1,13 +1,24 @@
 package com.ilovefundy.dto.funding;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import com.ilovefundy.dto.pay.PayInfo;
+import com.ilovefundy.dto.user.User;
+import lombok.*;
+import org.hibernate.annotations.BatchSize;
+import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
 
-@Data
+@JsonIdentityInfo(generator= ObjectIdGenerators.PropertyGenerator.class, property="fundingId")
+@Getter
+@Setter
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,6 +28,18 @@ public class FundingProject {
     @Column(name = "funding_id")
     private Integer fundingId;
 
+    @BatchSize(size=10)
+//    @JsonBackReference
+    @JsonIgnore
+    @ManyToMany(mappedBy = "fundings")
+    private Set<User> users = new LinkedHashSet<>();
+
+    @BatchSize(size=10)
+//    @JsonBackReference
+//    @JsonIgnore
+    @OneToMany(mappedBy = "funding")
+    private List<PayInfo> userPays = new ArrayList<>();
+
     @Column(name = "donation_id")
     private Integer donationId;
     @Column(name = "idol_id")
@@ -25,6 +48,7 @@ public class FundingProject {
     private Integer userId;
 
     @Column(name = "funding_type")
+    @ColumnDefault("1")
     private FundingType fundingType; // 펀딩 타입
     @Column(name = "funding_name")
     private String fundingName;
@@ -41,11 +65,14 @@ public class FundingProject {
     @Column(name = "funding_thumbnail")
     private String fundingThumbnail;
     @Column(name = "is_Donate")
+    @ColumnDefault("0")
     private boolean isDonate; // 기부 옵션 선택 여부
     @Column(name = "is_Confirm")
+    @ColumnDefault("0")
     private FundingConfirm isConfirm; // 펀딩 승인 여부
     @Column(name = "is_good_funding")
     @Enumerated(EnumType.STRING)
+    @ColumnDefault("'N'")
     private YesOrNo isGoodFunding; // 굿프로젝트 여부
 
     private enum FundingType{
