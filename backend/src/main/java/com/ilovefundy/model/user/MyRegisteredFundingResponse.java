6bf335e3b93577890f9a804cee7567f5b905c177
Yunsung <msmf3@naver.com>
2021-05-12 00:05:40 +0900

    🔨 [EDIT] FundingDao

--- backend/src/main/java/com/ilovefundy/model/user/MyRegisteredFundingResponse.java
+++ backend/src/main/java/com/ilovefundy/model/user/MyRegisteredFundingResponse.java
@@ -4,8 +4,10 @@ import com.ilovefundy.dto.funding.FundingProject;
 import com.ilovefundy.model.funding.FundingListResponse;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+@EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 @Data
 public class MyRegisteredFundingResponse extends FundingListResponse {
