cd76b195e9a853f17d52a5aad0e169030045e7a0
김재성 <kimjea23@naver.com>
2021-05-11 15:46:55 +0900

    :hammer: [EDIT] Update favorite idol components #S04P31A303-49

--- frontend/src/api/funding.ts
+++ frontend/src/api/funding.ts
@@ -1,3 +1,5 @@
-export const getFundingList = () => {
-    
+import axiosInstance from "./axiosConfig"
+
+export const getFavoriteFundingList = (user_id: number) => {
+    return axiosInstance.get('/user/' + user_id + "/my-funding");
 }
--- frontend/src/pages/mypage/Mypage.tsx
+++ frontend/src/pages/mypage/Mypage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import FundingList from '../../components/mypage/mypage/FundingList';
+import MyFunding from '../../components/mypage/mypage/MyFunding';
+import MyIdols from '../../components/mypage/mypage/MyIdols';
 import Profile from '../../components/mypage/mypage/Profile';
 
 const Mypage = () => {
   return (
     <div>
       <Profile />
-      <FundingList />
+      <MyFunding />
+      <MyIdols />
     </div>
   );
 };
