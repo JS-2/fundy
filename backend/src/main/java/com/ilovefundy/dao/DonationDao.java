4238e7dae066f3acd231a5b15038b032552f1161
Yunsung <msmf3@naver.com>
2021-05-12 17:58:02 +0900

    🔨 [EDIT] Donation API, Swagger - Response에 맞게 수정 #S04P31A303-71

--- backend/src/main/java/com/ilovefundy/dao/DonationDao.java
+++ backend/src/main/java/com/ilovefundy/dao/DonationDao.java
@@ -1,8 +1,25 @@
 package com.ilovefundy.dao;
 
 import com.ilovefundy.dto.donation.Donation;
+import com.ilovefundy.model.idol.IIdolDonation;
+import com.ilovefundy.model.idol.IIdolDonationRanking;
+import com.ilovefundy.model.idol.IdolDonationRankingResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface DonationDao extends JpaRepository<Donation, Integer> {
+    @Query(value = "SELECT d.idol_id as idolId, format(sum(d.idol_donation_amount),0) as donationAmount \n" +
+            "FROM donation d\n" +
+            "GROUP BY d.idol_id\n" +
+            "ORDER BY sum(d.idol_donation_amount) desc \n" +
+            "LIMIT 5;", nativeQuery = true)
+    List<IIdolDonationRanking> getTop5IdolDonationRanking();
 
+    @Query(value = "SELECT d.donation_place_id as donationPlaceId, d.idol_id as idolId, format(sum(d.idol_donation_amount),0) as donationAmount \n" +
+            "FROM donation d\n" +
+            "WHERE d.idol_id = ?1\n" +
+            "GROUP BY d.donation_place_id", nativeQuery = true)
+    List<IIdolDonation> getIdolDonationList(int idol_id);
 }
