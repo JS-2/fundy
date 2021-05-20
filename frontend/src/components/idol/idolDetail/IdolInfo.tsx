0f84a2c6cf2cab951e515be6094bdddf5c67e8f6
김재성 <kimjea23@naver.com>
2021-05-21 03:44:35 +0900

    :bug: [FIX] Refactor funding detail date

--- frontend/src/components/idol/idolDetail/IdolInfo.tsx
+++ frontend/src/components/idol/idolDetail/IdolInfo.tsx
@@ -224,7 +224,7 @@ const IdolInfo = (props: Props) => {
                             width: '100%',
                             height: '45px',
                             fontSize: '1.1em',
-                            opacity: idolFavorite ? 0.85 : 0.6,
+                            opacity: idolFavorite ? 0.85 : 0.85,
                           }}
                         >
                           관심 아이돌 {idolFavorite ? '해제' : '등록'}
